ba552527eb0a97b4558db28dcb9156ce54552996
XiM3RA <ddigler@protonmail.com>
2022-09-13 13:52:16 -0500

    Has a log of foods consumed. TODO: Custom food input, Recipe function, Archiving

--- Health.r
+++ Health.r
@@ -2,11 +2,16 @@
 
 Ingredients <- read.csv("DATA/Ingredients.csv")
 Log <- read.csv("DATA/Dailylog.csv")
+FoodLog <- data.frame(read.csv("DATA/Foodlog.csv", header=T, stringsAsFactor=FALSE))$id
+
 
 AddFood <- function(food, quantity) {
 	if (food %in% Ingredients$Food) {
 		entry <- subset(Ingredients, Food==food)[2:5]
 		Log[1,1:4] <<- Log[1,1:4] + (entry * quantity)
+        if (!food %in% FoodLog) {
+            FoodLog <<- append(FoodLog, food)
+        }
     }
 	else
 		print ("Invalid food selection, try again")
@@ -17,9 +22,13 @@ AddCals <- function(cals) {
 }
 
 ClearLog <- function() {
-	Log[1,] <<- c(0,0,0,0,0)
+	Log[1,] <<- c(0,0,0,0)
+    FoodLog <- c()
 }
 
 SaveLog <- function() {
 	write.csv(Log[1,], "DATA/DailyLog.csv", row.names=FALSE)
+    a <- data.frame(FoodLog)
+    colnames(a) <- c("id")
+    write.csv(a, "DATA/Foodlog.csv", row.names=FALSE)
 }
